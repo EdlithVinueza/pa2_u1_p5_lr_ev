1ac075b55c8eafd794446e00d31473b5ebe2f4c9
Josue Ocapana <josuensd@gmail.com>
2023-11-07 18:55:19 -0500

    mi primercommint

---
+++ src/main/java/com/uce/edu/Pa2U1P5LrEvApplication.java
@@ -0,0 +1,18 @@
+package com.uce.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+@SpringBootApplication
+public class Pa2U1P5LrEvApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U1P5LrEvApplication.class, args);
+	}
+	@Override
+	public void run(String...args) throws Exception{
+		System.out.println("Hola Mundo");
+		System.out.println("Soy Leonardo Ramirez y Edlith Vinueza");
+	}
+	
+}
