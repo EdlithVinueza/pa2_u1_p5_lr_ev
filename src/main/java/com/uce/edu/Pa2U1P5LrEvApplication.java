929ae791fb8f935b0a1beda9438685a7464d3b79
Edlith Vinueza <eavinuezaz1@uce.edu.ec>
2023-11-10 18:29:09 -0500

    cambio nuevo

--- src/main/java/com/uce/edu/Pa2U1P5LrEvApplication.java
+++ src/main/java/com/uce/edu/Pa2U1P5LrEvApplication.java
@@ -13,8 +13,9 @@ public class Pa2U1P5LrEvApplication implements CommandLineRunner{
 	public void run(String...args) throws Exception{
 		System.out.println("Hola Mundo");
 		System.out.println("Soy Leonardo Ramirez y Edlith Vinueza");
-		System.err.println("Cambio en la misma Rama");
+		System.out.println("Cambio en la misma Rama");
 		System.out.println("Cambio en una nueva rama taller 3");
+		System.err.println("Nuevo Cambio");
 	}
 	
 }
