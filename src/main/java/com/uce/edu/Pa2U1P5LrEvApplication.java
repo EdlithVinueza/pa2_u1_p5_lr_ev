35adf612912ba3c3394beb0fbbdd8c6fed10863e
Edlith Vinueza <eavinuezaz1@uce.edu.ec>
2023-11-13 19:44:36 -0500

    Taller 5

--- src/main/java/com/uce/edu/Pa2U1P5LrEvApplication.java
+++ src/main/java/com/uce/edu/Pa2U1P5LrEvApplication.java
@@ -2,21 +2,29 @@ package com.uce.edu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.uce.edu.ioc.di.Estudiante;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 @SpringBootApplication
 public class Pa2U1P5LrEvApplication implements CommandLineRunner{
-
+    
+	@Autowired
+	private Estudiante estudiante; 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U1P5LrEvApplication.class, args);
 	}
 	@Override
 	public void run(String...args) throws Exception{
-		System.out.println("Hola Mundo");
-		System.out.println("Soy Leonardo Ramirez y Edlith Vinueza");
-		System.out.println("Cambio en la misma Rama");
-		System.out.println("Cambio en una nueva rama taller 3");
-		System.out.println("Nuevo Cambio");
-		System.out.println("Taller 4 version 2");
+		this.estudiante.setCedula("1723353825 y 1751674027");
+		this.estudiante.setNombre("Leonardo y Edlith ");
+		this.estudiante.setApellido("Ramirez y Vinueza ");
+		this.estudiante.setSalario(new BigDecimal(1000));
+		System.out.println(estudiante);
 		
 	}
 	
